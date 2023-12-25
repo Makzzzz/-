07d1f3736f63fdef6c1813489c7a16c912c0ec6a
Makzzzz <147470769+Makzzzz@users.noreply.github.com>
2023-12-25 23:02:52 +0500

    Delete номер 5.py

--- номер 5.py
+++
@@ -1,7 +0,0 @@
-def shourtner(st):
-    while '('in st or')' in st:
-        left = st.rfind('(')
-        right = st.rfind(')', left)
-        st = st.replace(st[left:right + 1], '')
-        return st
-print(shourtner('Бамбимабамбум (5252525) ггвп ланая'))
