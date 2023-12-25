6e7f5c8252619ab3c955fdaad6634315d89e4b0c
Makzzzz <147470769+Makzzzz@users.noreply.github.com>
2023-12-25 21:38:02 +0500

    Add files via upload

---
+++ однокнопочный девайс.py
@@ -0,0 +1,15 @@
+a=''
+while True:
+    n=input()
+    if n=='e':
+        b='O'
+        a=a+b
+    elif n=='eeeee':
+        c='M'
+        a=a+c
+    elif n=='eeeeeee':
+        d='G'
+        a=a+d
+    else:
+        break
+print(a)
